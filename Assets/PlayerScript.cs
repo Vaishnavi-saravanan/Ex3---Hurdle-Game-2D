cf05fa3ec65e30b0a72619392f7cb5933a8a9f66
VaishnaviSaravanan <118541897+Vaishnavi-saravanan@users.noreply.github.com>
2024-03-23 10:32:55 +0530

    Update PlayerScript.cs

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -1,1 +1,39 @@
+using Systen;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
+public class PlayerScript : MonoBehaviour
+{
+    public float speed;
+    public float jumpforce;
+    private Rigidbody2D rb;
+    public CoinManager cc;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveinp = Input.GetAxisRaw("Horizontal");
+        transform.position += new Vector3(moveinp, 0, 0) * speed * Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.Space) && Mathf.Abs(rb.velocity.y) < 0.001f)
+        {
+            rb.AddForce(new Vector2(0, jumpforce), ForceMode2D.Impulse);
+        }
+git remote add origin "git clone....."
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("destroy"))
+        {
+            cc.coincount++;
+            Destroy(collision.gameObject);
+        }
+    }
+}
